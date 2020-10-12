581fcc5e9b39796cbd9dd5aae4ba379210a1aea2
nikola-margit-mqsoftrs <nikola.margit@mqsoft.rs>
2020-10-12 16:11:01 +0200

    Updates

---
+++ precache-manifest.d65b3758b5ee247841b536087c6eca47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc6a58e5556c0c76e160718c09b9a46",
+    "url": "/client-notification/index.html"
+  },
+  {
+    "revision": "0d81988bf309ddeec11d",
+    "url": "/client-notification/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "792fb88139bd5265277f",
+    "url": "/client-notification/static/css/main.4c48667a.chunk.css"
+  },
+  {
+    "revision": "0d81988bf309ddeec11d",
+    "url": "/client-notification/static/js/2.0253083c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/client-notification/static/js/2.0253083c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "792fb88139bd5265277f",
+    "url": "/client-notification/static/js/main.33ed40b6.chunk.js"
+  },
+  {
+    "revision": "c46462768cb704ea6157",
+    "url": "/client-notification/static/js/runtime-main.4042437d.js"
+  },
+  {
+    "revision": "7dfff396de1931bf3b464b7b6266cd58",
+    "url": "/client-notification/static/media/unilogo.7dfff396.png"
+  }
+]);
